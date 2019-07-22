1812794010465f38dc749930d36897de024a8cd9
chaso13 <charlesrheavner@gmail.com>
2019-07-22 04:10:11 -0400

    implemented routing for the navbar with react-router-dom NavLinks instead of href so as to not refresh the browser when the page is changed via navigation

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import { Image, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { Image, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { NavLink} from 'react-router-dom';
 
 export default class CustomNavbar extends Component {
     render() {
         return (
             <div>
                 <Navbar collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
-                  <Navbar.Brand href="#home"><Image style={{ height: '6vh', width: '10vw' }} src="assets/Logoo.png" /></Navbar.Brand>
+                  <Navbar.Brand><NavLink to="/"><Image style={{ height: '6vh', width: '10vw' }} src="assets/Logoo.png" /></NavLink></Navbar.Brand>
                   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                   <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto">
-                      <Nav.Link href="/">Home</Nav.Link>
-                      <Nav.Link href="/CurrentlySupporting">Currently Supporting</Nav.Link>
+                      <Nav.Link><NavLink to="/">Home</NavLink></Nav.Link>
+                      <Nav.Link><NavLink to="/CurrentlySupporting">Currently Supporting</NavLink></Nav.Link>
                       </Nav>
                       <Nav>
                         <NavDropdown title="Services" id="collasible-nav-dropdown">
-                          <NavDropdown.Item href="/Hosting">Hosting</NavDropdown.Item>
-                          <NavDropdown.Item href="/WebDevelopment">Web Development</NavDropdown.Item>
-                          <NavDropdown.Item href="/WebsiteMaintanence">Maintanence</NavDropdown.Item>
-                          <NavDropdown.Item href="/AppDevelopment">App Development</NavDropdown.Item>
+                          <NavDropdown.Item><NavLink to="/Hosting">Hosting</NavLink></NavDropdown.Item>
+                          <NavDropdown.Item><NavLink to="/WebDevelopment">Web Development</NavLink></NavDropdown.Item>
+                          <NavDropdown.Item><NavLink to="/WebsiteMaintanence">Maintanence</NavLink></NavDropdown.Item>
+                          <NavDropdown.Item><NavLink to="/AppDevelopment">App Development</NavLink></NavDropdown.Item>
                           <NavDropdown.Divider />
-                          <NavDropdown.Item href="/AllServices">All Services</NavDropdown.Item>
+                          <NavDropdown.Item><NavLink to="/AllServices">All Services</NavLink></NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="/AboutUs">About Us</Nav.Link>
+                        <Nav.Link><NavLink to="/AboutUs">About Us</NavLink></Nav.Link>
                       </Nav>
                     </Navbar.Collapse>
                   </Navbar>
