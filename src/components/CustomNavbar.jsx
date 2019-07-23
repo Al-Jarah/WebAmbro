24c43a3430e87469140ada5949d7b5c602cbe013
chaso13 <charlesrheavner@gmail.com>
2019-07-23 18:28:17 -0400

    added the top background and started working on the navbars styling

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { Image, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { NavLink,Link} from 'react-router-dom';
+import { NavLink} from 'react-router-dom';
 import "./CustomNavbar.scss";
 
 export default class CustomNavbar extends Component {
     render() {
         return (
             <div>
-                <Navbar collapseOnSelect fixed="top" expand="false" bg="dark" variant="dark">
-                <Navbar.Brand className="navbar-brand"><NavLink to="/"><Image style={{ height: '7vh', width: '3vw' }} src="assets/Logo.png" /><Image style={{ height: '6vh', width: '10vw' }} src="assets/Logoo.png" /></NavLink></Navbar.Brand>
+                <Navbar collapseOnSelect fixed="top" expand="false" className="navbar" variant="dark" >
+                <Navbar.Brand className="navbar-brand"><NavLink to="/"><Image className="logo-nav" src="assets/Logo.png" /><Image className="logoo-nav" src="assets/Logoo.png" /></NavLink></Navbar.Brand>
                   <Navbar.Toggle className="navbar-toggle" aria-controls="responsive-navbar-nav" />
                   <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import LazyHero from 'react-lazy-hero';
+import { Image,Container,Row, Column, Jumbotron } from 'react-bootstrap';
 import HostingServices from './homeelements/HostingServices';
+import "./Home.scss";
 
 export default class Home extends Component {
     render() {
+        const bgImage = "/assets/Blue-Background.jpg";
+        const styles = {
+            container: {
+            backgroundImage: `url(${bgImage})`,
+            backgroundSize: 'cover'
+          }
+        };
         return (
-            <div>
-                <div style={{ height: '100vh', width: '100vw' }}>
-                    {/*<LazyHero>
-                        <HostingServices />
-                    </LazyHero>*/}
+            <div className="home">
+                <div className="home-top">
+                    <Jumbotron className="home-top-jumbotron" style={styles.container}>
+                        <Image className="Logo" src="assets/Logo.png"/>
+                        <Image className="Logoo"src="assets/Logoo.png"/>
+                        
+                    </Jumbotron>
                 </div>
-                <div style={{ height: '100vh', width: '100vw' }}>
-                    
-                </div>
+               
             </div>
         )
     }
