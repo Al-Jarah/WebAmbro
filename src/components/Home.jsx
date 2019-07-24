361706efce07aded8355fe0d19d99b6b03d09fc5
chaso13 <charlesrheavner@gmail.com>
2019-07-24 02:00:15 -0400

    working on home top background images and making things responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,12 +15,23 @@ export default class Home extends Component {
         };
         return (
             <div className="home">
+                <div className="overlay"></div>
                 <div className="home-top">
-                    <Jumbotron className="home-top-jumbotron" style={styles.container}>
+                    
+                    
+                        <Image className="home-top-background-image" src="assets/Light-Background6.jpg"/>
                         <Image className="Logo" src="assets/Logo.png"/>
                         <Image className="Logoo"src="assets/Logoo.png"/>
-                        
-                    </Jumbotron>
+                        <div className="jumbo-text">
+                        <p className="home-top-text">Looking for a fresh creative design, a new application, or maybe just want to integrate the newest technologies into your exsisting software.
+                          At Web Ambrosia we strive to create the best product possible by using the latest technologies,UX and UI standards, 
+                          and world class creative teams and developers. At Web Ambrosia we want to make your dream a reality, we offer custom development,
+                          stunning design, hosting for your website or application, SEO services to ensure your product
+                          is ranking well on Google, and all of that for some of the best prices in the industry. So whether you are looking to expand your
+                          current business and bring in more customers, start up a new business, host your website or application, have an application made for ios/android ,or we even do in house business 
+                          software. Whatever your need choose Web Ambrosia for quality products and services fit for a God.</p>
+                        </div>
+                    
                 </div>
                
             </div>
